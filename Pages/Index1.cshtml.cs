625cc5e7cec49337f0d67cc5714c301b1d46ad31
Jeff-Pozo <pozojefferson410@gmail.com>
2024-09-19 00:41:29 -0600

    Agregar archivos de proyecto.

---
+++ Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_01_BD.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
