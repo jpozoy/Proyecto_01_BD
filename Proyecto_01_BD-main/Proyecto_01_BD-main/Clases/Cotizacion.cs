73b05bb08175043f5bd096801681bce1a31b65c9
Jeff-Pozo <pozojefferson410@gmail.com>
2024-09-24 20:10:26 -0600

    Conexion de la bd

---
+++ Proyecto_01_BD-main/Proyecto_01_BD-main/Clases/Cotizacion.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_01_BD.Clases
+{
+    public class Cotizacion
+    {
+        public string Codigo_Cotizacion { get; set; }
+        public string Cliente { get; set; }
+
+        public decimal Monto_Total { get; set; }
+    }
+}
---
+++ Proyecto_01_BD-main/Proyecto_01_BD-main/ConexionBD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Proyecto_01_BD
+{
+    public class ConexionBD
+    {
+        // Cadena de conexión
+        string connectionString = "Server=DESKTOP-L1JAUHL\\SQLEXPRESS;Database=SistemaERP;User Id=sa;Password=12345678;";
+
+        public SqlConnection conectarbd = new SqlConnection();
+
+        public ConexionBD()
+        {
+            conectarbd.ConnectionString = connectionString;
+        }
+
+        // Método para abrir la conexión
+        public void Abrir()
+        {
+            try
+            {
+                conectarbd.Open();
+                Console.WriteLine("Conexion abierta");
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Error al abrir la BD" + ex.Message);
+            }
+        }
+
+        // Método para cerrar la conexión
+        public void Cerrar()
+        {
+            conectarbd.Close();
+
+        }
+    }
+}
