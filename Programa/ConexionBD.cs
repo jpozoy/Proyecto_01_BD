962db175d60807b753e08151c29f8001213185f7
Jeff-Pozo <pozojefferson410@gmail.com>
2024-10-16 14:24:02 -0600

    Configuración de string de conexion

--- Programa/ConexionBD.cs
+++ Programa/ConexionBD.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Data.SqlClient;
-
+using Microsoft.Extensions.Configuration; 
+using System.IO; 
 namespace Proyecto_01_BD
 {
     public class ConexionBD
     {
         // Cadena de conexión
-        string connectionString = "Server=DESKTOP-L1JAUHL\\SQLEXPRESS;Database=SistemaERP;User Id=sa;Password=12345678;";
-
-        public SqlConnection conectarbd = new SqlConnection();
+        private string connectionString;
+        public SqlConnection conectarbd;
 
         public ConexionBD()
         {
-            conectarbd.ConnectionString = connectionString;
+            // Crear el objeto Configuration para leer el archivo appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Obtener el string de conexión
+            connectionString = configuration.GetConnectionString("ERPConnection");
+
+            // Inicializar el objeto SqlConnection con el string de conexión
+            conectarbd = new SqlConnection(connectionString);
         }
 
         // Método para abrir la conexión
@@ -21,18 +31,26 @@ namespace Proyecto_01_BD
             try
             {
                 conectarbd.Open();
-                Console.WriteLine("Conexion abierta");
-            } catch (Exception ex)
+                Console.WriteLine("Conexión abierta");
+            }
+            catch (Exception ex)
             {
-                Console.WriteLine("Error al abrir la BD" + ex.Message);
+                Console.WriteLine("Error al abrir la BD: " + ex.Message);
             }
         }
 
         // Método para cerrar la conexión
         public void Cerrar()
         {
-            conectarbd.Close();
-
+            try
+            {
+                conectarbd.Close();
+                Console.WriteLine("Conexión cerrada");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cerrar la BD: " + ex.Message);
+            }
         }
     }
 }
