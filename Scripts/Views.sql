531ef80f6b95b3fef0670913866bebfdd50f2285
Hillary Malespin Ulloa <80055697+HillaryMalespin@users.noreply.github.com>
2024-10-23 21:54:23 -0600

    Vista para ver articulos por bodega Views.sql

--- Scripts/Views.sql
+++ Scripts/Views.sql
@@ -6,3 +6,19 @@ from Usuario
 go
 
 select * from Listado_Usuarios
+
+CREATE VIEW Vista_ArticulosPorBodega
+AS
+SELECT 
+    b.Codigo_Bodega, 
+    b.Nombre AS Nombre_Bodega, 
+    a.Codigo_Articulo, 
+    a.Nombre_Articulo, 
+    i.Cantidad
+FROM 
+    Inventario i
+JOIN 
+    Articulo a ON i.Codigo_Articulo = a.Codigo_Articulo
+JOIN 
+    Bodega b ON i.Codigo_Bodega = b.Codigo_Bodega;
+GO
